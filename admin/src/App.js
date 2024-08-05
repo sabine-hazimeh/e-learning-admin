d4af473a4364d6ca6a17c797f0f79c292d0f97b0
sabine hazimeh <130570511+sabine-hazimeh@users.noreply.github.com>
2024-08-05 14:47:13 +0300

    adding create class form

--- admin/src/App.js
+++ admin/src/App.js
@@ -4,6 +4,7 @@ import Header from "./Header";
 import Home from "./Home";
 import Login from "./Login";
 import SignUp from "./Sign Up";
+import Classes from "./Classes";
 function App() {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/classes" element={<Classes />} />
       </Routes>
     </BrowserRouter>
   );
